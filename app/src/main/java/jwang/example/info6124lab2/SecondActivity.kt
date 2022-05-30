c811f2594e2fcdbf2f38778ad95ea6e8dff1d883
Jianwei Wang <j_wang68715@fanshaweonline.ca>
2022-05-30 15:19:23 -0400

    Initial.

---
+++ app/src/main/java/jwang/example/info6124lab2/SecondActivity.kt
@@ -0,0 +1,11 @@
+package jwang.example.info6124lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
---
+++ app/src/main/java/jwang/example/info6124lab2/ThirdActivity.kt
@@ -0,0 +1,11 @@
+package jwang.example.info6124lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third)
+    }
+}
